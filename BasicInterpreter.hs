3e959be129b6038e373410166064912f480cc304
lnunno <lucasnunno@gmail.com>
2014-02-20 22:15:40 -0700

    Binding reading.

--- BasicInterpreter.hs
+++ BasicInterpreter.hs
@@ -5,3 +5,22 @@ By: Lucas Nunno
 -}
 module BasicInterpreter where
 
+import BasicParser
+
+type Binding = (Variable,Int)
+
+readInt :: IO Int
+readInt = readLn
+
+readInput :: Statement -> IO [Binding]
+readInput (Input exprs) = do
+    let strInt = 99999 :: Int
+    inLs <- sequence [case e of 
+                    (EString s) -> do
+                        putStr (s ++ " ")
+                        return (Variable "fake", strInt)
+                    (EVar v)    -> do
+                        i <- readInt
+                        return (v,i)
+                         | e <- exprs]
+    return $ filter (\(_,x) -> x /= strInt) inLs
