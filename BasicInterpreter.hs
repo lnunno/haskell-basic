254b74d7866f7561cdb62e0cc0cbf1302010c6f9
lnunno <lucasnunno@gmail.com>
2014-02-23 21:34:22 -0700

    Added State monad evaluations for let and next.

--- BasicInterpreter.hs
+++ BasicInterpreter.hs
@@ -7,12 +7,35 @@ module BasicInterpreter where
 
 import BasicParser
 import System.Random
+import Control.Monad.State
 import qualified Data.Map as Map
 
 type Binding = (Variable,Int)
 
 type Context = Map.Map Variable Int
 
+statefulInsert :: Variable -> Int -> State Context ()  
+statefulInsert k v = state $ \xs -> ((),Map.insert k v xs) 
+
+ctxStateEx :: State Context ()
+ctxStateEx = do
+    ctx <- get
+    statefulInsert (Variable "X") 5
+    statefulInsert (Variable "Y") 3
+
+evalLet :: Statement -> State Context ()
+evalLet (Let var (ENum i)) = do
+    statefulInsert var i
+
+evalNext :: Statement -> State Context ()
+evalNext (NextIter (EVar v)) = do
+    ctx <- get 
+    let previousValue = ctx Map.! v
+    statefulInsert v (previousValue+1)
+
+evalExpr :: Expression -> Int
+evalExpr (ENum i) = i
+    
 readInt :: IO Int
 readInt = readLn
 
