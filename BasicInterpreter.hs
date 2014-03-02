55b026c5b833ab662b015e6a4e290d257c48e3f5
Lucas <lucasnunno@gmail.com>
2014-03-02 16:38:21 -0700

    Print support, eval abstraction with some tests.

--- BasicInterpreter.hs
+++ BasicInterpreter.hs
@@ -1,4 +1,68 @@
 module BasicInterpreter where
 
+import BasicParser
 import Control.Monad.State
 import qualified Data.Sequence as Seq
+import qualified Data.Map as Map
+
+type LineNumber = Int
+type Binding = (Variable,Int)
+type VariableMap = Map.Map Variable Int
+
+data BasicContext = BasicContext {
+    varMap :: VariableMap,
+    instructionPointer :: LineNumber,
+    loopStack :: [LineNumber]
+}
+
+initialContext = BasicContext Map.empty 0 []
+
+type Basic = StateT BasicContext IO ()
+
+putStrSp a = putStr (a ++ " ") 
+
+-- Print statement
+eval :: Statement -> Basic
+eval (Print exprs) = do
+    BasicContext vMap _ _ <- get
+    let ps = [case e of
+                    (EString s) -> do
+                        liftIO $ putStrSp s 
+                    (EVar v)    -> do
+                        liftIO $ putStrSp (show $ vMap Map.! v)
+                        | e <- exprs] 
+    let nl = [(liftIO $ putStrLn "")] :: [Basic]
+    sequence_ (ps ++ nl)
+
+-- Let statement
+eval (Let var (ENum i)) = do
+    BasicContext vMap ip ls <- get
+    put $ BasicContext (Map.insert var i vMap) ip ls
+
+eval' :: [Statement] -> Basic
+eval' stmts = do
+    sequence_ (map eval stmts)
+
+{-
+Tests 
+-}
+p1 = [Print [EString "Foo", EString "Bar"]]
+vp1 = [
+    Let (Variable "X") (ENum 3),
+    Let (Variable "Y") (ENum 5),
+    Print [
+        EString "X =", 
+        EVar (Variable "X"),
+        EString "Y =",
+        EVar (Variable "Y")
+        ]
+    ]
+
+tr a = runStateT a initialContext
+
+testPrint = do
+    tr $ eval' p1
+    tr $ eval' vp1
+
+main = do
+    testPrint
